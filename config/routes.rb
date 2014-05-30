fe2cd986d19046678820edfff1b5a69b54108164
KhoaThai <khoathai@misfitwearables.com>
2014-05-30 12:01:23 +0700

    add exams

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 MongodbDevise::Application.routes.draw do
+  get "exams/index"
   # get "users/show"
 
   devise_for :users
@@ -7,6 +8,7 @@ MongodbDevise::Application.routes.draw do
   
   resources :users
   resources :multiple_choice_questions
+  resources :exams
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
